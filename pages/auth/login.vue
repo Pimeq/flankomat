c0d08354d81fa6e72ca54af37aeea6e7900e95e5
Pimeq <rzepecki2@gmail.com>
2023-11-07 22:51:11 +0100

    Update redirect URL for Discord OAuth login

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -9,7 +9,7 @@
 		await supabase.auth.signInWithOAuth({
 			provider: "discord",
 			options: {
-				redirectTo: "http://localhost:3000/auth/confirm",
+				redirectTo: "http://flankomat.pimeq.systems/auth/confirm",
 			},
 		});
 	};
