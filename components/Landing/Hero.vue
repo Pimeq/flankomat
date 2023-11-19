d5aa416770c5deb7704bf73777612d42289fdc51
Pimeq <rzepecki2@gmail.com>
2023-11-19 23:24:49 +0100

    started working on the landing page

---
+++ components/Landing/Hero.vue
@@ -0,0 +1,24 @@
+<setup>
+    
+</setup>
+<template>
+	<div class="bg-gray-800">
+		<!-- Hero -->
+		<div class="hero p-10">
+			<div class="container mx-auto text-center">
+				<h1 class="text-5xl font-bold text-white mb-4">
+					Tired of borring alcoholism?
+				</h1>
+				<p class="text-xl text-gray-300 mb-6">
+					Get wasted together with your friends!
+				</p>
+				<button
+					href="#"
+					class="transition ease-in-out hover:-translate-y-1 hover:scale-110 duration-300 bg-green-400 hover:bg-emerald-400 text-gray-700 font-bold py-2 px-4 rounded-md"
+				>
+					Get Started
+				</button>
+			</div>
+		</div>
+	</div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -3,11 +3,46 @@
 </script>
 
 <template>
-	<div>
-		<h1>Nuxt Routing set up successfully!</h1>
-		<p>Current route: {{ route.path }}</p>
-		<a href="https://nuxt.com/docs/getting-started/routing" target="_blank"
-			>Learn more about Nuxt Routing</a
-		>
+	<div class="bg-slate-900 h-screen">
+		<div class="navbar">
+			<div class="container mx-auto">
+				<div class="flex justify-between">
+					<div class="flex space-x-4">
+						<NuxtLink
+							href="#"
+							class="flex items-center py-4 px-2 text-green-400 hover:text-green-700 transition-all ease-in-out duration-3"
+						>
+							<svg class="h-6 w-6 mr-1" viewBox="0 0 24 24">
+								<path
+									d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5v5a2 2 0 01-2 2H4a2 2 0 01-2-2v-5z"
+									fill="currentColor"
+								/>
+							</svg>
+							<span class="font-bold">Flankomat</span>
+						</NuxtLink>
+						<NuxtLink
+							href="#"
+							class="py-4 px-2 text-green-400 hover:text-green-700 transition-all ease-in-out duration-3 border-b-2 border-green-400"
+							>About</NuxtLink
+						>
+						<NuxtLink
+							href="#"
+							class="py-4 px-2 text-green-400 hover:text-green-700 transition-all ease-in-out duration-3 border-b-2 border-green-400"
+							>Pricing</NuxtLink
+						>
+						<NuxtLink
+							href="#"
+							class="py-4 px-2 text-green-400 hover:text-green-700 transition-all ease-in-out duration-3 border-b-2 border-green-400 font-bold"
+							>Contact Us</NuxtLink
+						>
+					</div>
+					<div class="flex items-center space-x-4">
+						<UButton to="/auth/login" class="py-2.5 px-5">Log in</UButton>
+					</div>
+				</div>
+			</div>
+		</div>
+		<LandingHero />
+		<div class="pricing"></div>
 	</div>
 </template>
