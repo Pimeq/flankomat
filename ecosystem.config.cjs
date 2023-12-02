029d4ffef1a5f6a1f37034628d6ad9adf735ee71
Hubert Rzepecki <54513008+Pimeq@users.noreply.github.com>
2023-12-02 19:54:00 +0100

    Update ecosystem.config.cjs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -7,9 +7,9 @@ module.exports = {
       instances: 'max',
       script: './.output/server/index.mjs',
       env: {
-        DATABASE_URL: process.env.DATABASE_URL,
-        SUPABASE_KEY: process.env.SUPABASE_KEY,
-        SUPABASE_URL: process.env.SUPABASE_URL,
+        "DATABASE_URL": process.env.DATABASE_URL,
+        "SUPABASE_KEY": process.env.SUPABASE_KEY,
+        "SUPABASE_URL": process.env.SUPABASE_URL,
       },
     }
   ]
